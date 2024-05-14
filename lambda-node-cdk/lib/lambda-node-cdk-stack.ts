b2d60d85fdee3d35aad68010d86d93d2232152ab
Michelangelo <m@michelangelo.codes>
2024-05-14 16:31:27 +0200

    add permissions and LambdaRestApi

--- lambda-node-cdk/lib/lambda-node-cdk-stack.ts
+++ lambda-node-cdk/lib/lambda-node-cdk-stack.ts
@@ -3,6 +3,8 @@ import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import { create } from "domain";
 
 export class LambdaNodeCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -12,9 +14,9 @@ export class LambdaNodeCdkStack extends Stack {
     const table = new Table(this, "MyTable", {
       partitionKey: { name: "id", type: AttributeType.STRING },
     });
+
     // Creates a new Node.js Lambda function, passing the table name as an environment variable
-    // missing, permissions to access the DynamoDB table
-    new NodejsFunction(this, "MyFunc", {
+    const getLambdaFunction = new NodejsFunction(this, "MyFunc", {
       runtime: Runtime.NODEJS_16_X,
       handler: "handler",
       entry: "lib/lambda-node.ts",
@@ -23,6 +25,11 @@ export class LambdaNodeCdkStack extends Stack {
       },
     });
 
-    // function code is looking for an api gateway event, but no api gateway is created
+    table.grantReadData(getLambdaFunction); // Grants the Lambda function read access to the DynamoDB table
+
+    // Creates a new API Gateway REST API with the Lambda function as the handler
+    const api = new LambdaRestApi(this, "MyApi", {
+      handler: getLambdaFunction,
+    });
   }
 }
