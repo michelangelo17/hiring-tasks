a5e668e74278289dd6843aa96351431fc00e2857
Michelangelo <m@michelangelo.codes>
2024-05-15 09:37:12 +0200

    added delete functionality

---
+++ lambda-node-cdk/lib/lambdas/deleteItem.ts
@@ -0,0 +1,41 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { DeleteItemCommand, DynamoDB } from "@aws-sdk/client-dynamodb";
+
+const tableName = process.env.TABLE_NAME;
+const region = process.env.REGION || "eu-central-1";
+const dynamoDbClient = new DynamoDB({ region });
+
+const deleteDynamoDBItem = async (itemId: string) => {
+  // deleteDynamoDBItem
+  const command = new DeleteItemCommand({
+    TableName: tableName,
+    Key: {
+      id: { S: itemId },
+    },
+    ReturnValues: "ALL_OLD",
+  });
+  return await dynamoDbClient.send(command);
+};
+
+const createResponse = (statusCode: number, body: Object): APIGatewayProxyResult => ({
+  statusCode,
+  body: JSON.stringify(body),
+});
+
+// Handler
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const itemId = event.pathParameters?.id;
+  if (!itemId) {
+    return createResponse(400, { message: "Item ID is missing" });
+  }
+
+  try {
+    const response = await deleteDynamoDBItem(itemId);
+    console.log(response);
+
+    return createResponse(200, { message: "ok - item found", data: response });
+  } catch (error) {
+    console.error(error);
+    return createResponse(500, { message: "ERROR something went wrong!", error });
+  }
+};
